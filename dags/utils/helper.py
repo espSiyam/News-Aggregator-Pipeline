c795e2e87cdcd088e3c24ac117597b11201e59a0
espsiyam <sohag35-292@diu.edu.bd>
2023-11-26 01:05:02 +0200

    save data by date

--- dags/utils/helper.py
+++ dags/utils/helper.py
@@ -5,12 +5,14 @@ import pandas as pd
 from utils.scraper import scrape_data_from_url
 from google.cloud import storage
 import os
+from datetime import datetime
 
 FILE_PATH = 'dags/data/urls_list.pkl'
 PROTHOM_ALO_URL_LIST = "dags/data/prothomalo_urls_list.pkl"
 JUGANTOR_URL_LIST = "dags/data/jugantor_urls_list.pkl"
 KALER_KANTHO_URL_LIST = "dags/data/kalerkantho_urls_list.pkl"
-TRANSFORMED_DATA_CSV = "dags/data/scraped_data.csv"
+date = datetime.now().strftime("%Y-%m-%d")
+TRANSFORMED_DATA_CSV = f"dags/data/scraped_data_{date}.csv"
 GCP_PROJECT_ID = 'concured-playground'
 os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = "dags/utils/news_gcp_project.json"
 
